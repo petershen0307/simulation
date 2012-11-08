11c94aa81a9b16a24757209666af4c76f2ec428a
Peter <petershen0307@gmail.com>
2012-11-08 15:32:54 +0800

    remove IndexTree_CF.h struct_indexTree_CF.h IndexTree_CF.cpp

--- include/IndexTree_CF.h
+++
@@ -1,7 +0,0 @@
-#ifndef INDEXTREE_CF_H
-#define INDEXTREE_CF_H
-
-void buildBroadcastSeq(std::vector<DataItem>& , std::vector<Sequence*>& );
-void deleteBcastseq(std::vector<Sequence*>& );
-
-#endif // INDEXTREE_CF_H
